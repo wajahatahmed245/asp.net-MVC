f8e78e2eca6c09f4c55a8e1c1770d5f5a4b7aa05
wajahat ahmed <wajahat.ahmed245@gmail.com>
2018-08-21 18:00:56 -0700

    till viwebag

---
+++ boilercode/boilercode/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace boilercode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string test(string id,string name )
+        {
+            return "id = " +id + " name = " + name;
+        }
+
+        public ActionResult test2(string id, string name)
+        {
+            //List<string> list1 = new List<string>;
+
+            ViewBag.countries=new List<string>() {
+                "pakistan",
+                "india",
+                "china",
+                "afghanistan"
+            };
+
+            return View();
+        }
+
+        public ActionResult About( )
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact( )
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
